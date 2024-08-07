2a29351b1dbd484a020e0e595fa87e5e475640c5
ivnpdrn1 <126903168+ivnpdrn1@users.noreply.github.com>
2024-08-07 10:17:05 -0400

    Showing Report Name & Folder

--- fie9-aws-lambda-code.py
+++ fie9-aws-lambda-code.py
@@ -16,8 +16,8 @@ def lambda_handler(event, context):
         <soapenv:Header/>
         <soapenv:Body>
             <ser:runReport>
-                <folderName>My Reports</folderName>
-                <reportName>James Test Matrix</reportName>
+                <folderName>1Call Control (IP) (Sunshine Life)</folderName>
+                <reportName>Vlookup Report export 1</reportName>
                 <criteria>
                     <time>
                         <end>2020-05-10T23:00:00.000-07:00</end>
